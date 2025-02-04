056894b31ac0e1987e970940cd22b5cd93f8d035
kayquesann <kayque.ferreirasan@gmail.com>
2025-02-04 20:53:43 -0300

    initialization commit

---
+++ src/test/java/com/example/simpleCrud/SimpleCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simpleCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
