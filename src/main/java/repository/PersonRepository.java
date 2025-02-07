b5b365f2027f7492b08fa0ae82093b1ffa109b1b
kayquesann <kayque.ferreirasan@gmail.com>
2025-02-07 17:12:21 -0300

    deleting PersonRepository

--- src/main/java/repository/PersonRepository.java
+++
@@ -1,8 +0,0 @@
-package repository;
-
-import model.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<Person, Integer> {
-    
-}
